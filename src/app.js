3573f0c57d792f9501e0aa39153e1a5ea7f7fa38
Viv <vivatey@gmail.com>
2023-03-01 03:16:04 +0000

    Updated the time

--- src/app.js
+++ src/app.js
@@ -1,37 +1,60 @@
-function displayTemperature(response) {
-    console.log(response);
-    console.log(response.data);
-    console.log(response.data.temperature.current);
-    console.log(response.data.city);
+function formatDate(timestamp) {
+  // calculate the date
 
-    let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let date = new Date(timestamp);
 
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = response.data.city;
-    
-    let descriptionElement = document.querySelector("#description");
-    descriptionElement.innerHTML = response.data.condition.description;
-
-    let humidityElement = document.querySelector("#humidity");
-    humidityElement.innerHTML = response.data.temperature.humidity;
-
-    let windElement = document.querySelector("#wind");
-    windElement.innerHTML = Math.round(response.data.wind.speed);
-
-
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
   }
 
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
+}
 
 
-  let apiKey = "e8t47038af810df0bb6dd6e04ac3o3b1";
+function displayTemperature(response) {
+  console.log(response);
+  console.log(response.data);
+  console.log(response.data.temperature.current);
+  console.log(response.data.city);
 
-  // let query = "London";
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
 
-  let units = "metric";
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
 
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=london&key=${apiKey}&units=${units}`;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
 
-  console.log(apiUrl);
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
 
-  axios.get(apiUrl).then(displayTemperature);
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
+
+
+}
+
+
+
+let apiKey = "e8t47038af810df0bb6dd6e04ac3o3b1";
+
+let units = "metric";
+
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=london&key=${apiKey}&units=${units}`;
+
+console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);
